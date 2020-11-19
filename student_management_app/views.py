1e6583e02640d29f90df5e77243d7fd3c472f027
trefl <marektrefler@gmail.com>
2020-11-19 13:08:41 +0100

    po 5

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -18,10 +18,16 @@ def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2> Method Not Allowed</h2>")
     else:
-        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))
-        if user!=None:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),
+                                         password=request.POST.get("password"))
+        if user != None:
             login(request, user)
-            return HttpResponseRedirect('/admin_home')
+            if user.user_type == "1":
+                return HttpResponseRedirect('/admin_home')
+            elif user.user_type == "2":
+                return HttpResponse("Staff login"+str(user.user_type))
+            else:
+                return HttpResponse("Student login"+str(user.user_type))
         else:
             messages.error(request, "Invalid Login details")
             return HttpResponseRedirect("/")
@@ -29,7 +35,7 @@ def doLogin(request):
 
 def GetUserDetails(request):
     if request.user != None:
-        return HttpResponse("User:"+request.user.email+"usertype: "+str(request.user.user_type))
+        return HttpResponse("User:" + request.user.email + "usertype: " + str(request.user.user_type))
     else:
         return HttpResponse("Please login First")
 
@@ -37,4 +43,3 @@ def GetUserDetails(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect("/")
-
