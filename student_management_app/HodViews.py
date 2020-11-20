b5927e0a5c556d4e956471702a50d0b5ce315059
trefl <marektrefler@gmail.com>
2020-11-20 20:17:44 +0100

    8 upload picture

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -1,6 +1,7 @@
 
 
 from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -75,6 +76,11 @@ def add_student_save(request):
         session_end = request.POST.get('session_end')
         course_id = request.POST.get('course')
         sex = request.POST.get('sex')
+
+        profile_pic = request.FILES['profile_pic']
+        fs = FileSystemStorage()
+        filename = fs.save(profile_pic.name, profile_pic)
+        profile_pic_url = fs.url(filename)
         try:
             user = CustomUser.objects.create_user(username=username, password=password, email=email,
                                                   last_name=last_name, first_name=first_name, user_type=3)
@@ -84,7 +90,7 @@ def add_student_save(request):
             user.students.session_start_year = session_start
             user.students.session_end_year = session_end
             user.students.gender = sex
-            user.students.profile_pic = ""
+            user.students.profile_pic = profile_pic_url
             user.save()
             messages.success(request, "Successfully Added Student ")
             return HttpResponseRedirect("add_student")
@@ -196,6 +202,14 @@ def edit_student_save(request):
         course_id = request.POST.get('course')
         sex = request.POST.get('sex')
 
+        if request.FILES['profile_pic']:
+            profile_pic = request.FILES['profile_pic']
+            fs = FileSystemStorage()
+            filename = fs.save(profile_pic.name, profile_pic)
+            profile_pic_url = fs.url(filename)
+        else:
+            profile_pic_url = None
+
         try:
             user = CustomUser.objects.get(id=student_id)
             user.first_name = first_name
@@ -211,6 +225,8 @@ def edit_student_save(request):
             student.gender = sex
             course = Courses.objects.get(id=course_id)
             student.course_id = course
+            if profile_pic_url != None:
+                student.profile_pic = profile_pic_url
             student.save()
 
             messages.success(request, "Successfully Edited Student ")
