8a84dcc81635a823747705726ffd8e953570468b
trefl <marektrefler@gmail.com>
2020-12-19 14:10:55 +0100

    po 20

--- student_management_app/StudentViews.py
+++ student_management_app/StudentViews.py
@@ -6,12 +6,38 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from student_management_app.models import Students, Courses, Subjects, CustomUser, Attendance, AttendanceReport, \
-    LeaveReportStudent, FeedBackStudent
+    LeaveReportStudent, FeedBackStudent, SessionYearModel
 
 
 def student_home(request):
-    return render(request, "student_template/student_home_template.html")
+    student_obj = Students.objects.get(admin=request.user.id)
+    attendance_total = AttendanceReport.objects.filter(student_id=student_obj).count()
+    attendance_present = AttendanceReport.objects.filter(student_id=student_obj, status=True).count()
+    attendance_absent = AttendanceReport.objects.filter(student_id=student_obj, status=False).count()
+    course = Courses.objects.get(id=student_obj.course_id.id)
+    subjects = Subjects.objects.filter(course_id=course).count()
+    subjects_data = Subjects.objects.filter(course_id=course)
+    session_obj = SessionYearModel.object.get(id=student_obj.session_year_id.id)
+    #class_room = OnlineClassRoom.objects.filter(subject__in=subjects_data, is_active=True, session_years=session_obj)
 
+    subject_name = []
+    data_present = []
+    data_absent = []
+    subject_data = Subjects.objects.filter(course_id=student_obj.course_id)
+    for subject in subject_data:
+        attendance = Attendance.objects.filter(subject_id=subject.id)
+        attendance_present_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=True,
+                                                                   student_id=student_obj.id).count()
+        attendance_absent_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=False,
+                                                                  student_id=student_obj.id).count()
+        subject_name.append(subject.subject_name)
+        data_present.append(attendance_present_count)
+        data_absent.append(attendance_absent_count)
+
+    return render(request, "student_template/student_home_template.html",
+                  {"total_attendance": attendance_total, "attendance_absent": attendance_absent,
+                   "attendance_present": attendance_present, "subjects": subjects, "data_name": subject_name,
+                   "data1": data_present, "data2": data_absent})
 
 def student_view_attendance(request):
     student = Students.objects.get(admin=request.user.id)
@@ -51,7 +77,8 @@ def student_apply_leave_save(request):
         leave_msg = request.POST.get("leave_msg")
         student_obj = Students.objects.get(admin=request.user.id)
         try:
-            leave_report = LeaveReportStudent(student_id=student_obj, leave_date=leave_date, leave_message=leave_msg, leave_status=0)
+            leave_report = LeaveReportStudent(student_id=student_obj, leave_date=leave_date, leave_message=leave_msg,
+                                              leave_status=0)
             leave_report.save()
 
             messages.success(request, "Successfully Applied for Leave ")
@@ -59,7 +86,6 @@ def student_apply_leave_save(request):
         except:
             messages.error(request, "Failed To Apply for Leave ")
             return HttpResponseRedirect(reverse("student_apply_leave"))
-
 
 
 def student_feedback(request):
@@ -83,7 +109,6 @@ def student_feedback_save(request):
         except:
             messages.error(request, "Failed To Send Feedback")
             return HttpResponseRedirect(reverse("student_feedback"))
-
 
 
 def student_profile(request):
